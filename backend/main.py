d8aa70f03d04aaeb18c049f49de4aa5245397f6a
yummyPancake2607 <lakshit26v@gmail.com>
2025-09-05 16:39:18 +0530

    everything in backend

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
-from app.db import SessionLocal, engine
-from app import crud, schemas, models
-from app.auth import get_current_user
+from backend.db import SessionLocal, engine
+from backend import crud, schemas, models
+from backend.auth import get_current_user
 
 # =====================================================
 # FastAPI App
