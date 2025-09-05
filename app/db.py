3d30780bd1aac33695a8d0a52ca128c0ee5dd909
yummyPancake2607 <lakshit26v@gmail.com>
2025-09-05 15:51:05 +0530

    neon-setup

--- app/db.py
+++ app/db.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./expense_tracker.db"
+# Get Neon connection string from env variable
+raw_url = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Force psycopg driver
+SQLALCHEMY_DATABASE_URL = raw_url.replace("postgres://", "postgresql+psycopg://")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
